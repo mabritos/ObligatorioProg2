76832033cbdb11baf4a431fdac33b5bd9419c346
Matias Britos <mabritos@correo.um.edu.uy>
2018-06-08 15:40:32 -0300

    Subimos el TAD Hash

---
+++ src/uy/edu/um/prog2/adt/hash/ElementoYaExistenteException.java
@@ -0,0 +1,29 @@
+package uy.edu.um.prog2.adt.hash;
+
+public class ElementoYaExistenteException extends Exception {
+
+	public ElementoYaExistenteException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ElementoYaExistenteException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ElementoYaExistenteException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ElementoYaExistenteException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ElementoYaExistenteException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+}
